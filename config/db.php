72f4713db59d47ce4b0c365e8c607d8e8632a769
Anatoliy <anatoliy.rus@gmail.com>
2019-03-11 22:41:20 +0300

    Реализовано динамическое получение параметров для БД

--- config/db.php
+++ config/db.php
@@ -1,14 +1,27 @@
 <?php
 
+if (getenv("DATABASE_URL")) {
+    $url = parse_url(getenv("DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+} else {
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'art_store';
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_b59b517fd555530',
-    'username' => 'bdcfc1b415a131',
-    'password' => 'bc208bbe',
+    'dsn' => "mysql:host={$host};dbname={$dbname}",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
+    'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
