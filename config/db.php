a5bf7604d70ff84db267690a3d6687b69dfaad66
nboldar <nboldar1@gmail.com>
2019-03-11 20:53:18 +0300

    db credential added

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_b59b517fd555530',
+    'username' => 'bdcfc1b415a131',
+    'password' => 'bc208bbe',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
