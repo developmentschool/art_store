418a5d63c8e1c0abea58067de5d70a679eede687
Anatoliy <anatoliy.rus@gmail.com>
2019-03-12 12:13:04 +0300

    Реализована подгрузка локальной конфигурации БД из отдельного файла, который не попадает в репозиторий

--- config/db.php
+++ config/db.php
@@ -2,26 +2,22 @@
 
 if (getenv("DATABASE_URL")) {
     $url = parse_url(getenv("DATABASE_URL"));
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $dbname = substr($url["path"], 1);
-} else {
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'art_store';
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "mysql:host={$url["host"]};dbname=" . substr($url["path"], 1),
+        'username' => $url["user"],
+        'password' => $url["pass"],
+        'charset' => 'utf8',
+
+        // Schema cache options (for production environment)
+        'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
+}
+if (!file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'local_db.php')) {
+    die('Не найден файл локальной конфигурации базы банных "config/local_db.php"');
 }
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => "mysql:host={$host};dbname={$dbname}",
-    'username' => $username,
-    'password' => $password,
-    'charset' => 'utf8',
+return require __DIR__ . DIRECTORY_SEPARATOR . 'local_db.php';
 
-    // Schema cache options (for production environment)
-    'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
